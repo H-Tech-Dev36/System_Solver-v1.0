af0fb865df075db829fcdf1c631e04d24cf395d4
H-Tech-Dev36 <84352891+H-Tech-Dev36@users.noreply.github.com>
2021-07-25 00:04:21 +0200

    Add files via upload
    Openvas solvig TURNED ON [ OK ]

---
+++ systemSolver.py
@@ -0,0 +1,113 @@
+import time
+time.sleep( 2 ) 
+print(''' 
++ -- --=[  Distributed width no warenties    ]=-- -- +
++ -- --=[  This is an opensource script      ]=-- -- +
++ -- --=[  system solver 1v.                 ]=-- -- + 
++ -- --=[ Coded By H-Tech Dev 32 Admin       ]=-- -- +
++ -- --=[ And Directed By HackDevLeague      ]=-- -- +
++ -- --=[ Supervised and corrected by JBS3   ]=-- -- +
++ -- --=[ https://github.com/T-Tech-Dev-36   ]=-- -- +
++ -- --=[ SystemSolver v1.0 by T Tech Dev 36 ]=-- -- +
+''')
+time.sleep( 5 )
+print('''
+              -|---------------------|-
+              -[   Starting server   ]-''')
+print("              -[Run as Administrator!]- ")
+print("               ######################### ")
+print("               sending request to shell  ")
+time.sleep( 2 )
+print("           Type the administrator's password ")
+
+
+import os 
+
+print('''
+         [ SystemSolever Python3 distributed width no warenties ]
+
+                   [ ToolboX ]
+     
+     
+      [ E ]///////////EMERGENCY/////////[ E ]  <======== Use only at EMERGENCY (Someone have hacked you , APT errors System errors , 
+      [ 0 ]         Check ALL           [ 0 ]  <======== Check All isues ( All ) 
+      [ 1 ]   Solve metasploit errors   [ 1 ]  <======== Check if there are metasploit errors and solve it instantly 
+      [ 2 ]   Solve openvas/gvm errors  [ 2 ]  <======== Check OpenVAS and GVM's errors and solve instantly  
+      [ 3 ]           Credits           [ 3 ]  
+      
+      [ 4 ]       Complete Scann        [ 4 ]  <======== Scann the computer and display report 
+      ''')
+      
+opt = int(input(" Select an option : "))
+
+if opt == 2: 
+ 
+ print('''
+ installing openVAS /// Check if you are root !
+ ''') 
+ time.sleep( 2 )
+ print(" you ar e root")
+ time.sleep( 2 )
+ print(" R00T Ch€cKeD ")
+ time.sleep( 0.1 )
+ os.system(' sudo apt install openvas  ')
+ time.sleep( 1 )
+ print('''  solving problems !  ''') 
+ os.system(' sudo apt install gvm ')
+ time.sleep( 1 )
+ print(" installing dependencies and complementary modules  ")
+ time.sleep( 10 )
+ print(''' //////WARNING/////
+ 
+ BE ATENTIVE !
+ Your pass will be created after the solving !
+ 
+ GOOD LUCK ! 
+ 
+ 
+ 
+ ''')
+ os.system(' gvm-setup ') 
+ time.sleep( 5 )
+ print('''
+ Your pass is generating ! 
+ ''')
+ os.system(' sudo gvmd --user root --new-password 216858  ')
+ os.system('sudo greenbone-scapdata-sync ')
+ os.system('sudo greenbone-certdata-sync ')  
+ os.system('sudo gvm-check-setup  ')
+ print('''
+ 
+ _____________________________
+ |                           |
+                    
+                _.._
+              .'    '.
+             /   __   \ 
+          ,  |   ><   |  ,
+         . \  \      /  / .
+          \_'--`(  )'--'_/
+           .--'/(SO)'--.        <==== Solved !! Thank you for using SystemSolver
+           /  /` '' `\  \ 
+             |        |
+              \.    ./
+
+        === === === === === 
+ 
+        Openvas User : root
+       Openvas Pass : 216858 
+ 
+        === === === === === 
+ 
+ |___________________________| 
+ 
+ 
+ ''')
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
